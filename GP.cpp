516b0ec67c26e55c443827b2e8298e504b879adc
Mohamed Amine Tag <tag_mohamed_amine@yahoo.fr>
2025-10-01 06:32:11 +0100

    Update GP.cpp

--- GP.cpp
+++ GP.cpp
@@ -861,14 +861,14 @@ int main() {
     ostringstream stm ;
     stm<<n;
     string sn = stm.str();
-    string filename1 = "matDenNigF"+sn + ".dat";
-    string filename2 = "matNumSignF"+sn + ".dat";
-    string filename3 = "matCoefF"+sn + ".dat";
+    string filename1 = "matDenNigF-"+sn + ".dat";
+    string filename2 = "matNumSignF-"+sn + ".dat";
+    string filename3 = "matCoefF-"+sn + ".dat";
     WriteUint(filename1, matDenNigFinal);
     WriteUint(filename2, matNumSignFinal);
     WriteInt(filename3, matCoefFinal);
 
-    ofstream SPout("spanningTrees"+sn+".dat");
+    ofstream SPout("spanningTrees-"+sn+".dat");
     SPout << "[";
     for (size_t g = 0; g < spTr.size(); g++) {
         SPout << spTr[g];
@@ -877,7 +877,7 @@ int main() {
     SPout << "]";
     SPout.close();
 
-    ofstream SYM("symmetries"+sn+".txt");
+    ofstream SYM("symmetries-"+sn+".txt");
     SYM << "[";
     for (size_t g = 0; g < Sym.size(); g++) {
         SYM << Sym[g];
@@ -886,7 +886,7 @@ int main() {
     SYM << "]";
     SYM.close();
 
-    ofstream fout("graphs"+sn+".dat");
+    ofstream fout("graphs-"+sn+".dat");
     fout << "[";
     for (size_t g = 0; g < allGraphs.size(); g++) {
         fout << "[";
@@ -900,7 +900,7 @@ int main() {
     fout << "]";
     fout.close();
 
-    ofstream refDEN("refDenominators"+sn+".dat");
+    ofstream refDEN("refDenominators-"+sn+".dat");
     refDEN << "[";
     for(size_t g = 0; g < refDen.size(); g++) {
         refDEN << "[";
